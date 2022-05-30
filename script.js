20e1318ee996d68142efe6725215b0bce9709830
Guidzs <guidz.2004@gmail.com>
2022-05-30 15:02:33 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-const framePixels = document.getElementById("pixel-board")
-let frameDimension = [5,5]
+const framePixels = document.getElementById("pixel-board");
+let frameDimension = [5, 5];
+const paleteColors = document.getElementsByClassName("color");
+
+paleteColors[0].style.backgroundColor = 'black';
+paleteColors[1].style.backgroundColor = 'red';
+paleteColors[2].style.backgroundColor = 'pink';
+paleteColors[3].style.backgroundColor = 'green';
+
 window.onload = function () {
- 
+
   function createFrame() {
-    for (let heigth = 1; heigth <= frameDimension[0];heigth += 1 ){
-      for (let width = 1; width <= frameDimension[1];width += 1 ){
-        let pixel = document.createElement('div')
-        pixel.className = 'pixel'
-        framePixels.appendChild(pixel)
+    for (let heigth = 1; heigth <= frameDimension[0]; heigth += 1) {
+      for (let width = 1; width <= frameDimension[1]; width += 1) {
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        pixel.addEventListener('click', paint);
+        framePixels.appendChild(pixel);
       }
-      let Break = document.createElement('br')
-      framePixels.appendChild(Break)
+      let Break = document.createElement('br');
+      framePixels.appendChild(Break);
     }
   }
-  createFrame()
+  createFrame();
+
+  for (let ind = 0; ind < paleteColors.length; ind += 1) {
+    paleteColors[ind].addEventListener('click', colorSeletor);
+  }
+}
+
+function colorSeletor(event) {
+  for (let i = 0; i < paleteColors.length; i += 1) {
+    paleteColors[i].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+function paint(event) {
+  let colorOfPalete = document.getElementsByClassName("selected");
+  let getCss = window.getComputedStyle(colorOfPalete);
+  let colorBg = getCss.getPropertyValue("background-color");
+
+  event.target.style.backgroundColor = colorBg;
 }
